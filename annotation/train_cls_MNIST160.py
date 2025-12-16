6b9b5cbcf27c08a2572928078bf2dc34fa556c2b
Levin Jian <jianzhirong@gmail.com>
2025-12-16 13:11:24 +0800

    classify view angles

---
+++ annotation/train_cls_MNIST160.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Load a model
+# model = YOLO("yolo11n-cls.yaml")  # build a new model from YAML
+model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
+# model = YOLO("yolo11n-cls.yaml").load("yolo11n-cls.pt")  # build from YAML and transfer weights
+
+# Train the model
+results = model.train(data="mnist160", epochs=10, imgsz=64)
+# Validate the model
+metrics = model.val()  # no arguments needed, dataset and settings remembered
+metrics.top1  # top1 accuracy
+metrics.top5  # top5 accuracy
