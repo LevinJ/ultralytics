33ed7a70d2d2b7e6df6890cf6c2b6dd56b6a436a
UltralyticsAssistant <web@ultralytics.com>
2026-03-16 04:15:28 +0000

    Auto-format by https://ultralytics.com/actions

--- annotation/flip_and_show.py
+++ annotation/flip_and_show.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Hardcoded image path
-img_path = '/media/levin/DATA/checkpoints/controlnet/data/EOL2/18.02.2025/right/20250303_162900_7e49525e-d440-49b9-b905-d7ae864e5ee4.jpg'
+img_path = "/media/levin/DATA/checkpoints/controlnet/data/EOL2/18.02.2025/right/20250303_162900_7e49525e-d440-49b9-b905-d7ae864e5ee4.jpg"
 
 # Read the image
 img = cv2.imread(img_path)
@@ -18,13 +18,13 @@ img_flipped = cv2.flip(img_rgb, 1)
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.imshow(img_rgb)
-plt.title('Original')
-plt.axis('off')
+plt.title("Original")
+plt.axis("off")
 
 plt.subplot(1, 2, 2)
 plt.imshow(img_flipped)
-plt.title('Horizontally Flipped')
-plt.axis('off')
+plt.title("Horizontally Flipped")
+plt.axis("off")
 
 plt.tight_layout()
 plt.show()
